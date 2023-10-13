4a755c73bc026b39865154eaaf98007b19adf014
dieudv <dieu.cit@gmail.com>
2023-10-13 12:45:07 -0700

    Upgrade implementions

--- app/src/main/java/org/lzdev/oldtv/MainActivity.kt
+++ app/src/main/java/org/lzdev/oldtv/MainActivity.kt
@@ -1,7 +1,7 @@
 package org.lzdev.oldtv
 
+import android.annotation.SuppressLint
 import android.app.Activity
-import android.app.AlertDialog
 import android.content.*
 import android.media.MediaPlayer
 import android.net.ConnectivityManager
@@ -10,24 +10,26 @@ import android.net.Uri
 import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.util.Log
-import android.view.KeyEvent
-import android.widget.ArrayAdapter
-import kotlinx.android.synthetic.main.activity_main.*
-import java.io.BufferedReader
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.net.URL
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.VideoView
 
 
 class MainActivity : Activity() {
-    private val playlists: ArrayList<Channel> = ArrayList()
-    private lateinit var sharedPref: SharedPreferences
-    private var index = 0
+    //    private val playlists: ArrayList<Channel> = ArrayList()
+//    private lateinit var sharedPref: SharedPreferences
+//    private var index = 0
+    private lateinit var webView: WebView
+    private var currentVideoURL = ""
+    private lateinit var videoView: VideoView
 
     private var mWifiReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             if (isNetworkConnected()) {
-                loadChannels()
+                webView.loadUrl("https://vtvgo.vn")
+//                loadChannels()
             }
         }
     }
@@ -35,26 +37,58 @@ class MainActivity : Activity() {
     private val mOnErrorListener: MediaPlayer.OnErrorListener =
         MediaPlayer.OnErrorListener { _, _, _ ->
             if (isNetworkConnected()) {
-                playlists.removeAt(index)
-                playChannel()
-                video_view.start()
+//                playlists.removeAt(index)
+//                playChannel()
+                videoView.start()
             }
             true
         }
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        videoView = findViewById(R.id.video_view)
+
+        WebView.setWebContentsDebuggingEnabled(true)
+        webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.userAgentString = getString(R.string.user_agent)
+        webView.webViewClient = object : WebViewClient() {
+
+            override fun shouldInterceptRequest(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): WebResourceResponse? {
+                runOnUiThread {
+                    if (request != null) {
+                        val url = request.url.toString()
+                        if (url.indexOf(".m3u8") > 0) {
+                            if (currentVideoURL != url) {
+                                Log.e(TAG, "request URL = ${request.url}")
+                                currentVideoURL = url
+                                videoView.setVideoURI(Uri.parse(url))
+                                videoView.start()
+                            }
+                        }
+                    }
+                }
+                return super.shouldInterceptRequest(view, request)
+            }
+        }
+        webView.loadUrl("https://vtvgo.vn")
+
         registerWifiReceiver()
 
-        sharedPref = this.getPreferences(Context.MODE_PRIVATE)
-        index = getIndex()
-        video_view.setOnErrorListener(mOnErrorListener)
-        video_view.start()
-
+//        sharedPref = this.getPreferences(Context.MODE_PRIVATE)
+//        index = getIndex()
+        videoView.setOnErrorListener(mOnErrorListener)
+//        video_view.start()
+//
         if (isNetworkConnected()) {
-            loadChannels()
+            webView.loadUrl("https://vtvgo.vn")
+//            loadChannels()
         }
     }
 
@@ -67,6 +101,7 @@ class MainActivity : Activity() {
                     hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
                         result = true
                     }
+
                     hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
                         result = true
                     }
@@ -84,107 +119,109 @@ class MainActivity : Activity() {
         this.registerReceiver(mWifiReceiver, filter)
     }
 
-    private fun loadChannels() {
-        Thread {
-            val url = URL(getString(R.string.m3u_url)).openConnection()
-            val inputStream: InputStream = url.getInputStream()
-            val reader = BufferedReader(InputStreamReader(inputStream, "UTF-8"))
-            playlists.clear()
-            var channel = Channel()
-            while (true) {
-                val line = reader.readLine() ?: break
-                if (line.contains("#EXTINF")) {
-                    channel = Channel()
-                    channel.name = line.split(",").toTypedArray()[1]
+    /*
+        private fun loadChannels() {
+            Thread {
+                val url = URL(getString(R.string.m3u_url)).openConnection()
+                val inputStream: InputStream = url.getInputStream()
+                val reader = BufferedReader(InputStreamReader(inputStream, "UTF-8"))
+                playlists.clear()
+                var channel = Channel()
+                while (true) {
+                    val line = reader.readLine() ?: break
+                    if (line.contains("#EXTINF")) {
+                        channel = Channel()
+                        channel.name = line.split(",").toTypedArray()[1]
+                    }
+                    if (line.contains("http")) {
+                        channel.url = line
+                        playlists.add(channel)
+                    }
                 }
-                if (line.contains("http")) {
-                    channel.url = line
-                    playlists.add(channel)
-                }
-            }
 
-            inputStream.close()
-            runOnUiThread {
-                playChannel()
-            }
-        }.start()
-    }
-
-    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
-        Log.d(TAG, "onKeyUp $keyCode")
-        when (keyCode) {
-            KeyEvent.KEYCODE_DPAD_UP -> {
-                if (index < playlists.size - 1) {
-                    index++
-                } else {
-                    index = 0
-                }
-                return playChannel()
-            }
-            KeyEvent.KEYCODE_DPAD_DOWN -> {
-                if (index > 0) {
-                    index--
-                } else {
-                    index = playlists.size - 1
-                }
-                return playChannel()
-            }
-            KeyEvent.KEYCODE_DPAD_LEFT -> {
-            }
-            KeyEvent.KEYCODE_DPAD_RIGHT -> {
-            }
-            KeyEvent.KEYCODE_DPAD_CENTER -> {
-                val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-                val adapter = ArrayAdapter<String>(this, android.R.layout.select_dialog_item)
-                playlists.forEach {
-                    adapter.add(it.name)
-                }
-                builder.setSingleChoiceItems(adapter, index) { dialog, which ->
-                    index = which
+                inputStream.close()
+                runOnUiThread {
                     playChannel()
-                    dialog.dismiss()
                 }
-                builder.show()
+            }.start()
+        }
+
+        override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+            Log.d(TAG, "onKeyUp $keyCode")
+            when (keyCode) {
+                KeyEvent.KEYCODE_DPAD_UP -> {
+                    if (index < playlists.size - 1) {
+                        index++
+                    } else {
+                        index = 0
+                    }
+                    return playChannel()
+                }
+                KeyEvent.KEYCODE_DPAD_DOWN -> {
+                    if (index > 0) {
+                        index--
+                    } else {
+                        index = playlists.size - 1
+                    }
+                    return playChannel()
+                }
+                KeyEvent.KEYCODE_DPAD_LEFT -> {
+                }
+                KeyEvent.KEYCODE_DPAD_RIGHT -> {
+                }
+                KeyEvent.KEYCODE_DPAD_CENTER -> {
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                    val adapter = ArrayAdapter<String>(this, android.R.layout.select_dialog_item)
+                    playlists.forEach {
+                        adapter.add(it.name)
+                    }
+                    builder.setSingleChoiceItems(adapter, index) { dialog, which ->
+                        index = which
+                        playChannel()
+                        dialog.dismiss()
+                    }
+                    builder.show()
+                }
+                KeyEvent.KEYCODE_MENU -> {
+                    startActivityForResult(Intent(android.provider.Settings.ACTION_SETTINGS), 0)
+                }
+                KeyEvent.KEYCODE_BACK -> {
+                    loadChannels()
+                    return true
+                }
             }
-            KeyEvent.KEYCODE_MENU -> {
-                startActivityForResult(Intent(android.provider.Settings.ACTION_SETTINGS), 0)
+            return super.onKeyUp(keyCode, event)
+        }
+
+        private fun playChannel(): Boolean {
+            if (index >= playlists.size) {
+                index = 0
             }
-            KeyEvent.KEYCODE_BACK -> {
-                loadChannels()
-                return true
+            saveIndex(index)
+            video_view.setVideoURI(Uri.parse(playlists[index].url))
+            return true
+        }
+
+        private fun saveIndex(value: Int) {
+            with(sharedPref.edit()) {
+                putInt("id", value)
+                apply()
             }
         }
-        return super.onKeyUp(keyCode, event)
-    }
 
-    private fun playChannel(): Boolean {
-        if (index >= playlists.size) {
-            index = 0
+        private fun getIndex(): Int {
+            return sharedPref.getInt("id", 0)
         }
-        saveIndex(index)
-        video_view.setVideoURI(Uri.parse(playlists[index].url))
-        return true
-    }
-
-    private fun saveIndex(value: Int) {
-        with(sharedPref.edit()) {
-            putInt("id", value)
-            apply()
-        }
-    }
-
-    private fun getIndex(): Int {
-        return sharedPref.getInt("id", 0)
-    }
+    */
 
     override fun onPause() {
         super.onPause()
-        video_view.pause()
+        videoView.pause()
     }
 
     override fun onResume() {
         super.onResume()
-        video_view.start()
+        videoView.start()
     }
 
     companion object {
@@ -192,7 +229,8 @@ class MainActivity : Activity() {
     }
 }
 
+/*
 class Channel {
     lateinit var name: String
     lateinit var url: String
-}
+}*/
